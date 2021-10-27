3898bea33975decf2c7be62109f580e295432ce7
KapitanTygrysek <93029886+KapitanTygrysek@users.noreply.github.com>
2021-10-27 09:55:22 +0200

    Update baza.sql

--- baza.sql
+++ baza.sql
@@ -34,21 +34,6 @@ CREATE TABLE `uzytkownicy` (
   `email` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
---
--- Zrzut danych tabeli `uzytkownicy`
---
-
-INSERT INTO `uzytkownicy` (`id`, `login`, `haslo`, `email`) VALUES
-(29, 'izak', '$2y$10$t9La92nSRODbt1rP3qcM1O2zV769jnLlEVrIW3BPBau9tocFTX9F6', 'izak@onet.pl'),
-(30, 'piterox.xlolx@onet.pl', '$2y$10$QaxzCvjrcIlV7..jNqBIL.0Kh9GgTZburgF0qbqfMYOOISo5ZhIeu', 'asd'),
-(31, 'asd', '$2y$10$aNnqOVDZUoV5hgmp9z5qbeZZZ8hAPWEcVxdu/Slm/zobLvX7zyyuW', 'kapitan.tygrysek0001@onet.pl'),
-(32, 'a', '$2y$10$hAuB52rC7DScubALVky0ZODh10f712iVXbgKcY0l1xTUVSHHpz3lC', '1'),
-(33, '5', '$2y$10$e0TjsS5cmHvRtM6fgY6Qo.aaHpGTjdYgWbvoSJHu70ZqDpA/Y5Phe', '4'),
-(34, '1', '$2y$10$dpbkPSsw7YyrIy8p1ukaheCBtdIgUUxSka1yO3too0gtyy3OMD5w.', '54'),
-(35, 'tygrysek', '$2y$10$8cKOz9SLsbBYHGzuurvaF.ZYOhTgp0FRQMqXCbCkHe6erdXvK9jge', 'kaptygrysek@gmail.com'),
-(36, 'Adam', '$2y$10$119xOEcpDcYx5CQ8gimO9.mtB0qMQ2eFE1Ve25Jgw0ybHLLEj.DIi', '123123123'),
-(37, 'asdasd', '$2y$10$3vnzkMl3rc8rm486tASa5uAnP3cewdhZqRJgzH7wRyIwexaV33uoa', '111'),
-(38, '123', '$2y$10$3YYxHQh.IR4BIb4X6yTmWuCc4sJgzHt.s04Pc6Wu5wrEBAXF.NEki', '3321');
 
 -- --------------------------------------------------------
 
@@ -64,14 +49,7 @@ CREATE TABLE `wiad` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_polish_ci;
 
 --
--- Zrzut danych tabeli `wiad`
---
 
-INSERT INTO `wiad` (`id`, `wiadomosc`, `uzytkownik_id`, `uzytkownik_id2`) VALUES
-(93, 'asdadasdasdsaasdasdasdsa', 43, 32),
-(94, 'jansfjasnifasiufbsaiufbuiasfisanfoiasnfoas', 38, 32),
-(95, 'test', 38, 29),
-(96, 'Siema', 38, 31);
 
 --
 -- Indeksy dla zrzutów tabel
